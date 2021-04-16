0aee8ee9143d82f49875172e64f47afaf4f9d6dd
async-devil <async.devil@gmail.com>
2021-04-16 23:14:50 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Header = styled.h1`
+	margin: 2rem;
+	font-size: 4rem;
+`;
+
+function App() {
+	return (
+		<div id="app">
+			<Header>It works!</Header>
+		</div>
+	);
+}
+
+export default App;
