4290b7252e680e31a5ae8fd177e1a875744d2468
async-devil <async.devil@gmail.com>
2021-07-11 17:11:52 +0300

    test: updated test name and fixed linter error

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 import { render, screen } from "@testing-library/react";
+
 import App from "../App";
 
-test("renders learn react link", () => {
+test("Renders it works", () => {
 	render(<App />);
 	const linkElement = screen.getByText(/It works!/i);
 	expect(linkElement).toBeInTheDocument();
