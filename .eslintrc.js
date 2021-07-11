0bd7f384d1a54c2476b22be62d4861aa685a3817
async-devil <async.devil@gmail.com>
2021-07-11 17:12:58 +0300

    chore: updated eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+const mainRules = {
+	"import/order": [
+		1,
+		{
+			groups: ["builtin", "external", "internal", ["parent", "sibling"]],
+			pathGroups: [
+				{
+					pattern: "react",
+					group: "external",
+					position: "before",
+				},
+			],
+			pathGroupsExcludedImportTypes: ["react"],
+			"newlines-between": "always",
+			alphabetize: {
+				order: "asc",
+				caseInsensitive: true,
+			},
+		},
+	],
+
+	"jsx-quotes": [1, "prefer-double"],
+	"linebreak-style": 0,
+	"no-tabs": 0,
+	"implicit-arrow-linebreak": 0,
+	"operator-linebreak": 0,
+	"object-curly-newline": 0,
+
+	"no-nested-ternary": 0,
+	"no-bitwise": 1,
+
+	"react/jsx-indent": 0,
+	"react/jsx-indent-props": 0,
+	"react/no-array-index-key": 1,
+	"react/require-default-props": 0,
+	"react/jsx-curly-newline": 0,
+};
+
+const typescriptRules = {
+	"@typescript-eslint/comma-dangle": 0,
+	"@typescript-eslint/indent": 0,
+	"@typescript-eslint/quotes": 0,
+	"@typescript-eslint/no-unused-vars": 1,
+	"@typescript-eslint/comma-dangle": 0,
+};
+
+module.exports = {
+	extends: [
+		"eslint:recommended",
+		"prettier",
+
+		"plugin:import/recommended",
+		"plugin:import/typescript",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+
+		"plugin:sonarjs/recommended",
+		"plugin:promise/recommended",
+	],
+	ignorePatterns: ["lib/", ".eslintrc.js", "jest.config.js"],
+	rules: mainRules,
+	plugins: ["prettier", "import", "react", "react-hooks", "sonarjs", "promise"],
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "tsconfig.json",
+				tsconfigRootDir: ".",
+			},
+			extends: [
+				"plugin:@typescript-eslint/eslint-recommended",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"airbnb-typescript",
+			],
+			plugins: ["@typescript-eslint"],
+			rules: Object.assign(typescriptRules, mainRules),
+		},
+		{
+			files: "*.js",
+			parser: "esprima",
+		},
+	],
+};
